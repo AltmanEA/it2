0463fcff450ef90509de8bc8bb466e423f854b30
Альтман Евгений Анатольевич <-@omgups.ru>
2021-05-14 16:31:06 +0600

    exam

--- deploy.bat
+++ deploy.bat
@@ -3,9 +3,9 @@ hugo
 SET COURSE=it2
 SET CURDRIVE=%CD:~0,3%
 
-ROBOCOPY "%CURDRIVE%work\%COURSE%\public"   "%CURDRIVE%Work\AltmanEA.github.io\%COURSE%" /S
+ROBOCOPY "%CURDRIVE%work\edu\%COURSE%\public"   "%CURDRIVE%Work\AltmanEA.github.io\%COURSE%" /S
 
-RD /S /Q "%CURDRIVE%work\%COURSE%\public\"
+RD /S /Q "%CURDRIVE%work\edu\%COURSE%\public\"
 
 git add *
 git commit -m %1
